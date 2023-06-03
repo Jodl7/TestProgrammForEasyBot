be7d5f6376770ddada2522404ffc0f15f6bd6011
Jodl7 <115723821+Jodl7@users.noreply.github.com>
2023-06-03 19:40:20 +0800

    v1

--- src/main/java/com/vk/jodl7/TestProgrammForEasyBot/controllers/GoodsController.java
+++ src/main/java/com/vk/jodl7/TestProgrammForEasyBot/controllers/GoodsController.java
@@ -1,36 +1,44 @@
 package com.vk.jodl7.TestProgrammForEasyBot.controllers;
 
-import com.vk.jodl7.TestProgrammForEasyBot.DTO.EmptyObject;
-import org.springframework.ui.Model;
+import com.vk.jodl7.TestProgrammForEasyBot.DTO.GoodsDTO;
+import com.vk.jodl7.TestProgrammForEasyBot.models.Goods;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-//import javax.validation.Valid;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/goods")
 public class GoodsController {
 
     @GetMapping("/{id}")
-    public String showById(@PathVariable Long id) {
+    public String findById(@PathVariable Long id) {
         System.out.println("working " + id);
         return "showById";
     }
 
     @GetMapping()
-    public String showAll() {
-        return "/showAll";
+    public List<GoodsDTO> findByProductType() {
+        return Goods;
     }
 
     @PostMapping("/new")
-    public String save(@RequestBody EmptyObject empty) {
-        System.out.println(empty);
+    public String save(@RequestBody @Valid GoodsDTO goodsDTO,
+                       BindingResult bindingResult) {
+        System.out.println(goodsDTO);
         return "new created";
     }
 
     @PatchMapping("/update")
-    public String update(@RequestBody EmptyObject empty) {
-        System.out.println(empty);
+    public String update(@RequestBody GoodsDTO goodsDTO) {
+        System.out.println(goodsDTO);
         return "updute";
     }
+
+    private Goods convertToGoods (GoodsDTO goodsDTO) {
+        return modelMapper.map(goodsDTO, Goods.class);
+    }
+
 }
